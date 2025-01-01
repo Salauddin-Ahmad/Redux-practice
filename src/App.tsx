eeb567f8beeaf5ea6d37a298ae488ea6cd173aaf
Md. Salauddin <salauddinahmed555@gmail.com>
2025-01-01 13:32:07 +0600

    Use State and Fix TypeScript Errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
-import { useDispatch } from 'react-redux'
 import './App.css'
 import { decrement, increment } from './redux/features/counter/counterSlice';
+import { useAppDsipatch, useAppSelector } from './redux/hook';
+
 
 function App() {
 
-  const dispatch = useDispatch();
+  const dispatch = useAppDsipatch();
+
+  const {count} = useAppSelector((state) => state.counter);
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -19,7 +22,7 @@ function App() {
       <div>
         <h1> Counter with Redux</h1>
         <button onClick={handleIncrement}>Increment</button>
-        <div>0</div>
+        <div className=''>{count}</div>
         <button onClick={handleDecrement}>Decrement</button>
 
       </div>
